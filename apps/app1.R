5362a97d24f9b900ff30e74efb6edbac4a427376
Oscar Reyes <oscar.reyes@star-it.co>
2015-03-23 18:37:33 -0500

    New Version

---
+++ apps/app1.R
@@ -0,0 +1,20 @@
+require (rCharts)
+
+datainfo <- reactive ({
+        switch (input$dataset,
+                "iris" = as.data.frame(iris),
+                "mtcars" = as.data.frame(mtcars),
+                "trees" = as.data.frame(trees)
+        )
+})
+
+strsum <- reactive ({
+        switch (input$type,
+                "Summary" = summary (datainfo()),
+                "Structure" = str (datainfo())
+        )
+})
+
+output$sumstr <- renderPrint ({
+        strsum()
+})
